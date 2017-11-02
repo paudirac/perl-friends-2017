a0f9cfd1a719825a2725d1e16997318082398eb3
Pau Cervera i Badia <pau.cervera@capside.com>
2017-11-02 07:35:18 +0100

    Added helpers

--- code/prova-minima.fs
+++ code/prova-minima.fs
@@ -6,6 +6,13 @@ type Parsed =
 type Parser = string -> Parsed
 
 (* Utils *)
+
+let explode (s:string) = [for c in s -> c]
+let implode (xs:char list) =
+    let sb = System.Text.StringBuilder(xs.Length)
+    xs |> List.iter (sb.Append >> ignore)
+    sb.ToString()
+
 let stringify x = x.ToString();
 let printParsed = printfn "%A"
 let chopLeft (s:string) = s.[1..]
